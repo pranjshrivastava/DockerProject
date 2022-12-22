16092d2266684bd052df85b7a90d42a7a6c246bc
pranjshrivastava <102428888+pranjshrivastava@users.noreply.github.com>
2022-12-22 09:55:19 +0530

    Add files via upload

---
+++ about.java
@@ -0,0 +1,10 @@
+public class about {
+        public static void main(String [] args){
+            System.out.println("WELCOME TO DOCKERIZED JAVA APPLICATION.");
+            System.out.println("ABOUT RAMAIAH INSTITUTE OF TECHNOLOGY: \n\tRamaiah Institute of Technology (RIT), formerly known as M.S. Ramaiah Institute of Technology (MSRIT) \n\tis an autonomous private engineering college located in Bangalore in the Indian state of Karnataka. \n\tEstablished in 1962,he college is affiliated to Visvesvaraya Technological University.");
+            System.out.println("\n\n\n\n\n Thank You!");
+
+            
+        }
+}
+
